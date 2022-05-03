065b54bb700bfe024a48d2a8a6d60921bde0e9fd
Zoe Balmy <zbalmy@gmail.com>
2022-05-03 10:24:50 -0700

    added sum_even_numbers

--- 05.03.js
+++ 05.03.js
@@ -21,3 +21,31 @@ function get_array(num) {
 
 console.log(get_array(arrNum1));
 console.log(get_array(arrNum2));
+
+let evenLowSum1 = 1;
+const evenHighSum1 = 1000;
+//const expected = 250500
+
+let evenLowSum2 = 15;
+const evenHighSum2 = 150;
+//const expected = 5644
+
+/**
+ * given two numbers, return the sum of all even numbers between the two numbers
+ * @param {Number} low the lower limit of numbers
+ * @param {Number} high the upper limit of numbers
+ * @returns {Number} the sum of all the even numbers from low number to high number
+ */
+function sum_even_numbers(low, high) {
+  let sum = 0;
+  while (low <= high) {
+    if (low % 2 == 0) {
+      sum = sum + low;
+    }
+    low++;
+  }
+  return sum;
+}
+
+console.log(sum_even_numbers(evenLowSum1, evenHighSum1));
+console.log(sum_even_numbers(evenLowSum2, evenHighSum2));
