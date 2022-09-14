23002fc7d8778c36267aecf8beadd6b5f3de0a65
Zoe Balmy <zbalmy@gmail.com>
2022-09-14 14:30:43 -0700

    added comments

--- 05.04.js
+++ 05.04.js
@@ -1,30 +1,30 @@
-const oddLowSum1 = 1;
-const oddHighSum1 = 1000;
-//const expected = 250000
+// const oddLowSum1 = 1;
+// const oddHighSum1 = 1000;
+// //const expected = 250000
 
-const oddLowSum2 = 15;
-const oddHighSum2 = 150;
-//const expected = 5576
+// const oddLowSum2 = 15;
+// const oddHighSum2 = 150;
+// //const expected = 5576
 
-/**
- * given two numbers, return the sum of all odd numbers between the two numbers
- * @param {Number} low the lower limit of numbers
- * @param {Number} high the upper limit of numbers
- * @returns {Number} the sum of all the odd numbers from low number to high number
- */
-function sum_odd_numbers(low, high) {
-  let sum = 0;
-  while (low <= high) {
-    if (low % 2 == 1) {
-      sum = sum + low;
-    }
-    low++;
-  }
-  return sum;
-}
+// /**
+//  * given two numbers, return the sum of all odd numbers between the two numbers
+//  * @param {Number} low the lower limit of numbers
+//  * @param {Number} high the upper limit of numbers
+//  * @returns {Number} the sum of all the odd numbers from low number to high number
+//  */
+// function sum_odd_numbers(low, high) {
+//   let sum = 0;
+//   while (low <= high) {
+//     if (low % 2 == 1) {
+//       sum = sum + low;
+//     }
+//     low++;
+//   }
+//   return sum;
+// }
 
-console.log(sum_odd_numbers(oddLowSum1, oddHighSum1));
-console.log(sum_odd_numbers(oddLowSum2, oddHighSum2));
+// console.log(sum_odd_numbers(oddLowSum1, oddHighSum1));
+// console.log(sum_odd_numbers(oddLowSum2, oddHighSum2));
 
 //*************************************************************************/
 
@@ -37,24 +37,24 @@ const numArr2 = [1, 2, 5];
 const numArr3 = [-5, 2, 5, 12];
 //const expected = 14
 
-/**
- * given an array of numbers, return the sum of all the numbers in the array
- * @param {Array<Numbers>} arr an array of numbers
- * @returns {Number} the sum of all the numbers in the array
- */
-function iterArr(arr) {
-  let sum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    sum = sum + arr[i];
-  }
-  return sum;
-}
+// /**
+//  * given an array of numbers, return the sum of all the numbers in the array
+//  * @param {Array<Numbers>} arr an array of numbers
+//  * @returns {Number} the sum of all the numbers in the array
+//  */
+// function iterArr(arr) {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     sum = sum + arr[i];
+//   }
+//   return sum;
+// }
 
-console.log(iterArr(numArr1));
-console.log(iterArr(numArr2));
-console.log(iterArr(numArr3));
+// console.log(iterArr(numArr1));
+// console.log(iterArr(numArr2));
+// console.log(iterArr(numArr3));
 
-//********************************************************************** */
+// //********************************************************************** */
 const maxArr1 = [0, -2, 8];
 //const expected = 8
 
@@ -64,11 +64,11 @@ const maxArr2 = [300, 2, 200, 5];
 const maxArr3 = [1];
 //const expected = 1
 
-/**
- * given an array of numbers, return the highest number value inside the array
- * @param {Array<Numbers>} arr an array of numbers
- * @returns {Number} the highest number inside the given array
- */
+// /**
+//  * given an array of numbers, return the highest number value inside the array
+//  * @param {Array<Numbers>} arr an array of numbers
+//  * @returns {Number} the highest number inside the given array
+//  */
 function findMax(arr) {
   maxNum = arr[0];
   for (let i = 0; i < arr.length; i++) {
