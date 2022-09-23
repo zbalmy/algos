bcf59020d4252716ef3223d9997aff27e0c53e4b
Zoe Balmy <zbalmy@gmail.com>
2022-09-23 11:01:07 -0700

    initial commit

---
+++ 09.21.js
@@ -0,0 +1,46 @@
+//from 05.25.js
+
+const decodeStr1 = "a3b2c1d3";
+// const expected = "aaabbcddd";
+
+const decodeStr2 = "a3b2c12d10";
+// const expected = "aaabbccccccccccccdddddddddd";
+
+/**
+ * given an encoded string, return a string where each letter is repeated the
+ * amount of times of the number that follows it
+ * @param {String} str an encoded string
+ * @returns {String} a decoded string
+ */
+
+// function decode(str) {
+//   newStr = "";
+//   for (i = 0; i < str.length; i++) {
+//     for (j = 1; j <= str[i]; j++) {
+//       newStr = newStr + str[i - 1];
+//     }
+//   }
+//   return newStr;
+// }
+
+// console.log(decode(decodeStr1));
+// console.log(decode(decodeStr2)); // this one isn't working
+
+function decode2(str) {
+  let decoded = "";
+  let char;
+  let numStr = "";
+  for (let i = 0; i < str.length; i++) {
+    char = str[i];
+    while (!isNaN(str[i + 1])) {
+      i++;
+      numStr += str[i];
+    }
+    decoded += char.repeat(numStr);
+    numStr = "";
+  }
+  return decoded;
+}
+
+console.log(decode2(decodeStr1));
+console.log(decode2(decodeStr2));
